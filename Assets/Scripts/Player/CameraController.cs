53e3bfac753b785e418a4c84a07791b46c092b11
Jake <jaketreleaven@gmail.com>
2021-01-28 13:12:50 -0500

    HUD prefab and Player Movement
    Added HUD to prefabs and added mouse and keyboard functionality to playercontroller

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float sensitivity = 1000.0f;
+    public Transform playerBody;
+
+    //private float XRotation = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        //XRotation -= mouseY;
+        //XRotation = Mathf.Clamp(XRotation, -90.0f, 90.0f);
+
+        //transform.localRotation = Quaternion.Euler(XRotation, 0.0f, 30.0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float maxSpeed = 10.0f;
+    public float gravity = -30.0f;
+    public float jumpHeight = 10.0f;
+
+    public Joystick joystickMove;
+    public Joystick joystickLook;
+
+    public Transform groundCheck;
+    public float groundRadius = 0.5f;
+    public LayerMask groundMask;
+
+    private CharacterController character;
     // Start is called before the first frame update
     void Start()
     {
-        
+        character = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //float x = joystick.Horizontal;
+        //float z = joystick.Vertical;
+        Vector3 x = transform.forward * Input.GetAxisRaw("Vertical") * maxSpeed * Time.deltaTime;
+        Vector3 z = transform.right * Input.GetAxisRaw("Horizontal") * maxSpeed * Time.deltaTime;
+        Vector3 move = x + z;
+
+        character.Move(move);
     }
 }
