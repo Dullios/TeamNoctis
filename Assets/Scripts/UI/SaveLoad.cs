9d94a7506515b0129d370fbf7a270d7a4fefe5f5
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2021-03-19 06:42:57 -0400

    Change Loading

--- Assets/Scripts/UI/SaveLoad.cs
+++ Assets/Scripts/UI/SaveLoad.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class SaveLoad : MonoBehaviour
 {
@@ -16,6 +17,9 @@ public class SaveLoad : MonoBehaviour
 
     // tower lists
     public List<GameObject> towers;
+
+    // scene
+    public string loadScene;
 
     // Start is called before the first frame update
     void Start()
@@ -48,6 +52,16 @@ public class SaveLoad : MonoBehaviour
             {
                 Inventory.instance.LoadInventory();
             }
+
+            // load towers
+            //for (int i = 0; i < towers.Count; ++i)
+            //{
+            //    // position
+            //    towers[i].transform.position = new Vector3(PlayerPrefs.GetFloat("TowerPositionX"), PlayerPrefs.GetFloat("TowerPositionY"), PlayerPrefs.GetFloat("TowerPositionZ"));
+
+            //    //rotation
+            //    towers[i].transform.rotation = Quaternion.Euler(PlayerPrefs.GetFloat("TowerRotationX"), PlayerPrefs.GetFloat("TowerRotationY"), PlayerPrefs.GetFloat("TowerRotationZ"));
+            //}
         }
 
     }
@@ -81,9 +95,10 @@ public class SaveLoad : MonoBehaviour
         // find towers
         foreach (GameObject go in GameObject.FindGameObjectsWithTag("Tower"))
         {
+            // add to the list
             towers.Add(go);
+            // save tower list
 
-            // save towers
             if(towers != null)
             {
                 for(int i = 0; i < towers.Count; ++i)
@@ -109,27 +124,35 @@ public class SaveLoad : MonoBehaviour
 
     public void GoLoad()
     {
-        // load position
-        playerController.transform.position = new Vector3(PlayerPrefs.GetFloat("PositionX"), PlayerPrefs.GetFloat("PositionY"), PlayerPrefs.GetFloat("PositionZ"));
+        TitleButton.loadGame = true;
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(loadScene);
 
-        // load rotation - player (0,y,0)
-        playerController.transform.rotation = Quaternion.Euler(0, PlayerPrefs.GetFloat("RotationY"), 0);
 
-        // load rotation - player (x,0,0)
-        cameraController.XRotation = PlayerPrefs.GetFloat("RotationX");
 
-        // load player stats - HP & Stamina
-        stats.currnetHP = PlayerPrefs.GetFloat("HP");
-        stats.currentStamina = PlayerPrefs.GetFloat("STAMINA");
 
-        // load day night cycle
-        dayCycleManager.cycleCurrent = PlayerPrefs.GetFloat("DayNight");
 
-        // load inventory
-        if (Inventory.HasInstance) // check if there is inventory
-        {
-            Inventory.instance.LoadInventory();
-        }
+        //// load position
+        //playerController.transform.position = new Vector3(PlayerPrefs.GetFloat("PositionX"), PlayerPrefs.GetFloat("PositionY"), PlayerPrefs.GetFloat("PositionZ"));
+
+        //// load rotation - player (0,y,0)
+        //playerController.transform.rotation = Quaternion.Euler(0, PlayerPrefs.GetFloat("RotationY"), 0);
+
+        //// load rotation - player (x,0,0)
+        //cameraController.XRotation = PlayerPrefs.GetFloat("RotationX");
+
+        //// load player stats - HP & Stamina
+        //stats.currnetHP = PlayerPrefs.GetFloat("HP");
+        //stats.currentStamina = PlayerPrefs.GetFloat("STAMINA");
+
+        //// load day night cycle
+        //dayCycleManager.cycleCurrent = PlayerPrefs.GetFloat("DayNight");
+
+        //// load inventory
+        //if (Inventory.HasInstance) // check if there is inventory
+        //{
+        //    Inventory.instance.LoadInventory();
+        //}
     }
 
 }
