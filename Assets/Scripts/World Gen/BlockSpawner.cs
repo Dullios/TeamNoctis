446b2d2d9a6e0cf7c2027f116de3a837d9af26be
Russell Brabers <brabers.russell@gmail.com>
2021-03-20 12:57:20 -0400

    Combined Meshes
    - Chunk meshes and materials are combined when generated

--- Assets/Scripts/World Gen/BlockSpawner.cs
+++ Assets/Scripts/World Gen/BlockSpawner.cs
@@ -40,9 +40,19 @@ public class BlockSpawner : MonoBehaviour
     [Header("Chunk Values")]
     public Vector2 chunkPos;
 
+    [Header("Materials")]
+    public Material[] blockMat;
+
+    // Components
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
+        meshFilter = GetComponent<MeshFilter>();
+        meshRenderer = GetComponent<MeshRenderer>();
+
         // Grab values from ChunkManager Instance
         width = ChunkManager.Instance.width;
         height = ChunkManager.Instance.height;
@@ -54,7 +64,7 @@ public class BlockSpawner : MonoBehaviour
         perlinOffset = new Vector2(offset.x + (chunkPos.x * perlinStepSizeX), offset.y + (chunkPos.y * perlinStepSizeY));
 
         GenerateTerrain();
-
+        CreateCombinedMesh();
         GenerateNavMesh();
     }
 
@@ -115,6 +125,106 @@ public class BlockSpawner : MonoBehaviour
             (int)(y + perlinOffset.y)).grayscale;
 
         return sampledFloat;
+    }
+
+    //private void CreateCombinedMesh()
+    //{
+    //    Vector3 oldPos = transform.position;
+    //    transform.position = Vector3.zero;
+
+    //    meshRenderer.materials = blockMat;
+
+    //    MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+    //    MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>();
+    //    CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+
+    //    for (int i = 0; i < meshFilters.Length; i++)
+    //    {
+    //        if (meshFilters[i] == meshFilter)
+    //            continue;
+
+    //        combine[i].mesh = meshFilters[i].sharedMesh;
+    //        combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+    //        combine[i].subMeshIndex = 0;
+
+    //        meshFilters[i].GetComponent<MeshRenderer>().enabled = false;
+    //    }
+
+    //    meshFilter.mesh = new Mesh();
+    //    meshFilter.mesh.CombineMeshes(combine, true);
+
+    //    transform.position = oldPos;
+    //}
+
+    private void CreateCombinedMesh()
+    {
+        Vector3 oldPos = transform.position;
+        transform.position = Vector3.zero;
+
+        meshRenderer.materials = blockMat;
+
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>(false);
+        MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>(false);
+        List<Material> materialList = new List<Material>();
+
+        foreach(MeshRenderer renderer in meshRenderers)
+        {
+            if (renderer == meshRenderer)
+                continue;
+
+            Material[] localMats = renderer.sharedMaterials;
+            foreach(Material mat in localMats)
+            {
+                if (!materialList.Contains(mat))
+                    materialList.Add(mat);
+            }
+        }
+
+        List<Mesh> submeshes = new List<Mesh>();
+        foreach(Material mat in materialList)
+        {
+            List<CombineInstance> combiner = new List<CombineInstance>();
+
+            for (int i = 0; i < meshFilters.Length; i++)
+            {
+                Material[] localMat = meshRenderers[i].sharedMaterials;
+                for(int matIndex = 0; matIndex < localMat.Length; matIndex++)
+                {
+                    if (localMat[matIndex] != mat)
+                        continue;
+
+                    CombineInstance ci = new CombineInstance();
+                    ci.mesh = meshFilters[i].sharedMesh;
+                    ci.subMeshIndex = matIndex;
+                    ci.transform = meshFilters[i].transform.localToWorldMatrix;
+                    combiner.Add(ci);
+                }
+
+                if (meshRenderers[i] == meshRenderer)
+                    continue;
+                meshRenderers[i].enabled = false;
+            }
+
+            Mesh mesh = new Mesh();
+            mesh.CombineMeshes(combiner.ToArray(), true);
+            submeshes.Add(mesh);
+        }
+
+        List<CombineInstance> finalCombiners = new List<CombineInstance>();
+        foreach(Mesh mesh in submeshes)
+        {
+            CombineInstance ci = new CombineInstance();
+            ci.mesh = mesh;
+            ci.subMeshIndex = 0;
+            ci.transform = Matrix4x4.identity;
+            finalCombiners.Add(ci);
+        }
+
+        Mesh finalMesh = new Mesh();
+        finalMesh.CombineMeshes(finalCombiners.ToArray(), false);
+        meshFilter.sharedMesh = finalMesh;
+
+        transform.position = oldPos;
     }
 
     private void GenerateNavMesh()
